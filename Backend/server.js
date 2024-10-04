2857410c6ccd839483b71d90cdc83a2c71b48b8c
NTDK <99682074+NTDK5@users.noreply.github.com>
2024-10-04 22:33:09 +0300

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -25,7 +25,7 @@ app.use(helmet());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://dorze-tour.vercel.app/",
     methods: "GET,POST,PUT,DELETE", // Frontend URL
     credentials: true, // Allow credentials (cookies)
   })
