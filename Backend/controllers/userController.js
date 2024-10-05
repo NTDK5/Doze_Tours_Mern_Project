b10e28c6e473c2ff610a3c2f2ab7948106c73f98
NTDK <99682074+NTDK5@users.noreply.github.com>
2024-10-05 13:07:20 +0300

    Update userController.js

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -28,8 +28,8 @@ const authUser = asyncHandler(async (req, res) => {
     res.cookie("jwt", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
-      maxAge: 3600000,
+   sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+      maxAge: 30 * 24 * 60 * 60 * 1000,
     });
 
     res.status(200).json({
